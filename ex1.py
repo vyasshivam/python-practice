7843e85d539017cf3841e58355eff95492e8a90b
Shivam Vyas <shivamvyas34@gmail.com>
2017-09-21 12:29:16 -0400

    Completed extras 1

--- ex1.py
+++ ex1.py
@@ -2,6 +2,11 @@
 """
 Create a program that asks the user to enter their name and their age. 
 Print out a message addressed to them that tells them the year that they will turn 100 years old.
+
+Extras:
+
+1. Add on to the previous program by asking the user for another number and printing out that many 
+   copies of the previous message.
 """
 import datetime
 
@@ -9,5 +14,14 @@ name = input('Enter you name : ')
 age = int(input('Enter your age: '))
 
 currentYear = datetime.datetime.now().year
-print('In year %s %s will be 100 years old' % (str(currentYear - age + 100), name))
-                        
+message = 'In year %s %s will be 100 years old' % (str(currentYear - age + 100), name)
+print(message)
+
+totalCopies = int(input('How many times you want to print the above message'))
+
+#Print in one line
+print(totalCopies * message)
+
+#Print in new line
+for x in range(0,totalCopies):
+    print(message)
